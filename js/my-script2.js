2d2ec47387642bda0da242b97a46350d273763b0
alwaysSmile <sjuice@mail.ru>
2017-02-10 07:57:37 +0300

    s

--- js/my-script2.js
+++ js/my-script2.js
@@ -1,165 +1,33 @@
-
-//Ajax
-//Основы XMLHttpRequest
-//Объект XMLHttpRequest (кратко "XHR") даёт возможность из JavaScript
-//делать HTTP-запросы к серверу без перезагрузки страницы. Может работать с любыми данными
-//загружает файл phones.json из текущей директории и выдаёт его содержимое:
-
-//************
-//Создаём новый объект XMLHttpRequest
-//var xhr = new XMLHttpRequest();
-////Конфигурируем его: Get-запрос на url 'phones.json'
-//xhr.open('GET', 'jsons/phones.json', false);
-////Отсылаем запрос
-//xhr.send();
-////Если код ответа сервера не 200, то это ошибка
-//if (xhr.status != 200) {
-//    //обработать ошибку
-//    console.log(xhr.status + ': ' + xhr.statusText);//Пример вывода 404: Not Found
-//}
-//else {
-//    //вывести результат
-//    console.log(xhr.responseText);
-//}
-//************
-
-//************
-//Метод open, как правило вызывается первым после создания объекта XMLHttpRequest
-//Синтаксис:
-//xhr.open(metod, URL, async, user, password)
-
-//metod - HTTP-метод. Как правило используются GET или POST, хотя есть и более экзотические
-//TRACE/DELETE/PUT и т.п
-
-//URL - адрес запроса. Можно использовать не только http/https, но и другие протоколы,
-//например ftp:// и file
-
-//async - если установлено в false, то запрос производится синхронно, если true - асинхронно
-
-//"Синхронный запрос" означает, что после вызова xhr.send()
-//и до ответа сервера главный поток будет "заморожен":
-//посетитель не сможет взаимодействовать со страницей - прокручивать, нажимать на кнопочки и т.п
-//После получения ответа выполнение продолжится со следующей строки
-
-//"Асинхронный запрос" означает, что браузер отправит запрос,
-//а далее результат нужно будет получить через обработчик событий
-
-//user, password - логин и пароль для HTTP-авторизации, если нужны
-//************
-
-//вызов open, в противоположность своему названию (open – англ. «открыть») не открывает соединение.
-//Он лишь настраивает запрос, а коммуникация инициируется методом send.
-
-//************
-//Метод Send открывает соединение и отправляет запрос на сервер
-//Синтаксис:
-
-//xhr.send([body])
-
-//В body находится тело запроса. GET-запрос не имеет тела,
-//POST-запрос основные данные передаются через body
-//************
-
-//************
-//Отмена: abort
-//xhr.abort() - прерывает выполнение запроса
-//************
-
-//************
-//Ответ: status, statusText, responseText
-
-//status
-//HTTP-код ответа: 200, 404, 403. 0-Если сервер не ответил, или при запросе на другой домен.
-
-//statusText - текстовое описание статуса от сервера: OK, Not Found, Forbidden и так далее.
-
-//responseText - текст ответа сервера
-
-//responseXML
-//Если сервер вернул XML, снабдив его правильным заголовком Content-type: text/xml,
-//то браузер создаст из него XML-документ. По нему можно будет делать запросы xhr.responseXml.querySelector("...") и другие.
-//************
-
-//************
-
-var buttonRequest = document.getElementsByClassName('button-request')[0];
-
-function loadJson() {
-    var xhr = new XMLHttpRequest();
-    //Конфигурируем его: Get-запрос на url 'phones.json'
-    xhr.open('GET', 'jsons/phones.json', true);
-    //Отсылаем запрос
-    xhr.send();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState != 4)
-            return;
-        buttonRequest.innerHTML = 'Готово';
-
-        //Если код ответа сервера не 200, то это ошибка
-        if (xhr.status != 200) {
-            //обработать ошибку
-            console.log(xhr.status + ': ' + xhr.statusText);//Пример вывода 404: Not Found
-        } else {
-            //вывести результат
-            console.log(xhr.responseText);
-        }
-
-    };
-    buttonRequest.innerHTML = 'Загружаю';
-    buttonRequest.disabled = true;
-}
-;
-
-buttonRequest.onclick = loadJson;
-//************
-
-//************
-//Событие onreadystatechange происходит несколько раз в процессе отсылки и получения ответа.
-//При этом можно посмотреть текущее состояние запроса в свойстве xhr.readyState.
-//В примере выше мы использовали только состояние 4 (запрос завершён), но есть и другие
-
-//const unsigned short UNSENT = 0; // начальное состояние
-//const unsigned short OPENED = 1; // вызван open
-//const unsigned short HEADERS_RECEIVED = 2; // получены заголовки
-//const unsigned short LOADING = 3; // загружается тело (получен очередной пакет данных)
-//const unsigned short DONE = 4; // запрос завершён
-//************
-
-//************
-//Из примеров по node.js, только всё сплошным кодом
-function User(name) {
+// 1. Конструктор Animal
+function Animal(name) {
     this.name = name;
+    this.speed = 0;
 }
 
-User.prototype.hello = function (who) {
-    console.log(db.getPhrase("Hello") + ", " + who.name);
-};
+// 1.1. Методы -- в прототип
 
-var exports = User;
-var phrases = {
-    Hello: "Привет",
-    Run: "Запуск успешен"
+Animal.prototype.stop = function() {
+    this.speed = 0;
+    alert( this.name + ' стоит' );
 }
-console.log(phrases)
-//exports.connect - база данных загружает фразы
-exports.connect = function () {
 
+Animal.prototype.run = function(speed) {
+    this.speed += speed;
+    alert( this.name + ' бежит, скорость ' + this.speed );
 };
 
-exports.getPhrase = function (name) {
-    console.log(phrases[name]);
-    if (!phrases[name]) {
-        throw  new Error("Нет такой фразы " + name);
-    }
-    return phrases[name];
-};
+// 2. Конструктор Rabbit
+function Rabbit(name) {
+    this.name = name;
+    this.speed = 0;
+}
 
+// 2.1. Наследование
+Rabbit.prototype = Object.create(Animal.prototype);
+Rabbit.prototype.constructor = Rabbit;
 
-var db = exports;
-
-var vasya = new User("Вася");
-var petya = new User("Петя");
-
-vasya.hello(petya);
-//************
-
+// 2.2. Методы Rabbit
+Rabbit.prototype.jump = function() {
+    this.speed++;
+    alert( this.name + ' прыгает, скорость ' + this.speed );
+}
